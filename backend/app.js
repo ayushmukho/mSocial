74813ef6e436d9449fc677c2f71aa9b80ce0ba97
ayushmukho <ayushmukho@gmail.com>
2022-04-07 22:59:38 +0530

    Set Up schema for post amd user

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
+
+//import post Routes
+const postRoutes = require("./routes/post");
 
 module.exports = app;
