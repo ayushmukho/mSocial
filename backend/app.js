d3225b782b86018b3ed376cf5010274a784b933a
ayushmukho <ayushmukho@gmail.com>
2022-04-08 16:26:47 +0530

    Routes and Controllers

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,15 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-//import post Routes
+//Using Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//import Routes
 const postRoutes = require("./routes/post");
+const userRoutes = require("./routes/user");
+
+app.use("/api/v1", postRoutes);
+app.use("/api/v1", userRoutes);
 
 module.exports = app;
