455ae7abfc232cf19c8e035b717d388e259d7ae8
ayushmukho <ayushmukho@gmail.com>
2022-04-09 00:27:28 +0530

    Login and register --backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,11 +20,69 @@ exports.register = async (req, res) => {
       avatar: { public_id: "Sample Id", url: "Sample Url" },
     });
 
-    res.status(201).json({
+    // <--- Just Register --->
+
+    // res.status(201).json({
+    //   success: true,
+    //   user,
+    // });
+
+    // <--- Register and Login --->
+
+    const token = await user.generateToken();
+
+    const options = {
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+
+    res.status(201).cookie("token", token, options).json({
       success: true,
       user,
+      token,
     });
-    
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect password",
+      });
+    }
+
+    const token = await user.generateToken();
+
+    const options = {
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+
+    res.status(200).cookie("token", token, options).json({
+      success: true,
+      user,
+      token,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
