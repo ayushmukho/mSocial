4449fba63b7da821087a6e484fd575ace7c57943
ayushmukho <ayushmukho@gmail.com>
2022-04-10 12:31:04 +0530

    Follow and Unfollow user --backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -90,3 +90,49 @@ exports.login = async (req, res) => {
     });
   }
 };
+
+exports.followUser = async (req, res) => {
+  try {
+    const userToFollow = await User.findById(req.params.id);
+    const loggedInUser = await User.findById(req.user._id);
+
+    if (!userToFollow) {
+      return res.status(404).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    if (loggedInUser.following.includes(userToFollow._id)) {
+      const indexFollowing = loggedInUser.following.indexOf(userToFollow._id);
+      loggedInUser.following.splice(indexFollowing, 1);
+
+      const indexFollowers = userToFollow.followers.indexOf(loggedInUser._id);
+      userToFollow.followers.splice(indexFollowers, 1);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      res.status(200).json({
+        success: true,
+        message: "User Unfollowed",
+      });
+    } else {
+      loggedInUser.following.push(userToFollow._id);
+      userToFollow.followers.push(loggedInUser._id);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      res.status(200).json({
+        success: true,
+        message: "User Followed",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
