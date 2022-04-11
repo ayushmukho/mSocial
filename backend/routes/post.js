e14c700ffe36e142cd133a6666d61afa634637e4
ayushmukho <ayushmukho@gmail.com>
2022-04-11 11:01:54 +0530

    Update Post --backend

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const { createPost, likeAndUnlikePost, deletePosts, getPostOfFollowing } = require("../controllers/post");
+const {
+  createPost,
+  likeAndUnlikePost,
+  deletePosts,
+  getPostOfFollowing,
+  updateCaption,
+} = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
-router.route("/post/:id").get(isAuthenticated, likeAndUnlikePost).delete(isAuthenticated, deletePosts);
+router
+  .route("/post/:id")
+  .get(isAuthenticated, likeAndUnlikePost)
+  .put(isAuthenticated, updateCaption)
+  .delete(isAuthenticated, deletePosts);
+
 router.route("/posts").get(isAuthenticated, getPostOfFollowing);
 
 module.exports = router;
