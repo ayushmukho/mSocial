c1dac1cbe19340eea8c077ab449f4bf6536779cb
ayushmukho <ayushmukho@gmail.com>
2022-04-11 10:39:02 +0530

    update Profile --backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, followUser, logout } = require("../controllers/user");
+const { register, login, followUser, logout, updatePassword, updateProfile } = require("../controllers/user");
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
@@ -7,6 +7,10 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
+
 router.route("/follow/:id").get(isAuthenticated, followUser);
+
+router.route("/update/password").put(isAuthenticated, updatePassword);
+router.route("/update/profile").put(isAuthenticated, updateProfile);
 
 module.exports = router;
