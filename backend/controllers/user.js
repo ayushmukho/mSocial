ce2d440ffa461f67305594f4d818a80ceea5039f
ayushmukho <ayushmukho@gmail.com>
2022-04-12 14:25:06 +0530

    Get My profile --backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -249,3 +249,20 @@ exports.deleteMyProfile = async (req, res) => {
     });
   }
 };
+
+
+exports.myProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).populate("posts");
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
